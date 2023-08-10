cf9fef720ff1f2f42ab213c3a8a9afac085a3b50
SecondThundeR <awayfromgalaxy@gmail.com>
2023-08-11 00:34:48 +0300

    feat: add rate limit support

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,7 @@
 import "dotenv/config";
+import Redis from "ioredis";
 import { Bot } from "grammy";
+import { limit } from "@grammyjs/ratelimiter";
 
 import { cat } from "@/commands/cat";
 import { dog } from "@/commands/dog";
@@ -8,14 +10,39 @@ import { help } from "@/commands/help";
 import { weather } from "@/commands/weather";
 import { commandsData } from "@/constants/commandsData";
 
-if (!process.env.BOT_TOKEN) {
+const { BOT_TOKEN, REDISPORT, REDISHOST, REDISUSER, REDISPASSWORD } =
+  process.env;
+
+if (!BOT_TOKEN) {
   console.error("Bot token is empty!");
   process.exit(1);
 }
 
-const bot = new Bot(process.env.BOT_TOKEN);
+const redis = new Redis({
+  port: Number(REDISPORT),
+  host: REDISHOST,
+  username: REDISUSER,
+  password: REDISPASSWORD,
+});
+const bot = new Bot(BOT_TOKEN);
 
 bot.api.setMyCommands(commandsData);
+
+bot.use(
+  limit({
+    timeFrame: 2000,
+    limit: 3,
+    storageClient: redis,
+    onLimitExceeded: async (ctx) => {
+      await ctx.reply(
+        "You are sending too many requests! Please wait before sending new one"
+      );
+    },
+    keyGenerator: (ctx) => {
+      return ctx.from?.id.toString();
+    },
+  })
+);
 
 bot.use(start);
 bot.use(help);
--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -4,6 +4,10 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       BOT_TOKEN: string;
+      REDISHOST: string;
+      REDISPASSWORD: string;
+      REDISPORT: string;
+      REDISUSER: string;
       WEATHER_API_KEY: string;
       CAT_API_KEY: string;
       DOG_API_KEY: string;
