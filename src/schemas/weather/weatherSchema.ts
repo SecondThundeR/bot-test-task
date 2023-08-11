d77bd137bf0ab7497c87c02329d1724439ca1d27
SecondThundeR <awayfromgalaxy@gmail.com>
2023-08-11 15:03:22 +0300

    feat(schemas): add `location` object to weather schema

--- src/schemas/weather/weatherSchema.ts
+++ src/schemas/weather/weatherSchema.ts
@@ -1,6 +1,9 @@
 import z from "zod";
 
 export const WeatherResponseSchema = z.object({
+  location: z.object({
+    name: z.string(),
+  }),
   current: z.object({
     temp_c: z.number(),
     condition: z.object({
