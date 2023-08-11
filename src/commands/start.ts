861c9e403cd281a040f668bf506809eb6e2e7def
SecondThundeR <awayfromgalaxy@gmail.com>
2023-08-11 14:38:47 +0300

    fix(commands): add missing comma in start text

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -3,7 +3,7 @@ import { Composer } from "grammy";
 export const start = new Composer();
 
 start.command("start", (ctx) => {
-  ctx.reply("Welcome\\! To get more info about functionality type `/help`", {
+  ctx.reply("Welcome\\! To get more info about functionality, type `/help`", {
     parse_mode: "MarkdownV2",
   });
 });
