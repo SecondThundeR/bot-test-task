3a58e483feb8a72d696d8d08376250528ef4063a
SecondThundeR <awayfromgalaxy@gmail.com>
2023-08-15 10:56:43 +0300

    feat(types): add session data for context

--- src/types/bot.ts
+++ src/types/bot.ts
@@ -1,8 +1,18 @@
-import { type Context } from "grammy";
+import { type Context, type SessionFlavor } from "grammy";
 import {
   type Conversation,
   type ConversationFlavor,
 } from "@grammyjs/conversations";
 
-export type BotContext = Context & ConversationFlavor;
+import { type TodoEntry } from "@/store/user/todos";
+
+interface SessionData {
+  currentTodoList?: TodoEntry[] | null;
+  selectedTodo?: TodoEntry | null;
+  currentOffset: number;
+}
+
+export type BotContext = Context &
+  ConversationFlavor &
+  SessionFlavor<SessionData>;
 export type BotConversation = Conversation<BotContext>;
