c28a7e496a3750554bbfe884834bd01e35c68100
SecondThundeR <awayfromgalaxy@gmail.com>
2023-08-16 18:38:17 +0300

    fix: correct check for hours in `checkTimeFormat`

--- src/utils/checkTimeFormat.ts
+++ src/utils/checkTimeFormat.ts
@@ -2,7 +2,7 @@ export function checkTimeFormat(time?: string) {
   if (!time || time.length > 5) return false;
   const [hours, minutes] = time.split(":");
 
-  if (parseInt(hours) < 0 || parseInt(hours) > 24) return false;
+  if (parseInt(hours) < 0 || parseInt(hours) > 23) return false;
   if (parseInt(minutes) < 0 || parseInt(minutes) > 59) return false;
 
   return true;
