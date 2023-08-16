6f50490d4cd8a91a96449b2217d6700654cec729
SecondThundeR <awayfromgalaxy@gmail.com>
2023-08-16 19:20:28 +0300

    fix: rename `isValidDate` to `isValidNotificationDate`

--- src/utils/isValidNotificationDate.ts
+++ src/utils/isValidNotificationDate.ts
@@ -2,7 +2,7 @@ import moment from "moment";
 
 const DATE_FORMAT = "DD.MM.YYYY";
 
-export function isValidDate(date?: string) {
+export function isValidNotificationDate(date?: string) {
   if (!date) return false;
 
   const parsedDate = moment(date, DATE_FORMAT);
