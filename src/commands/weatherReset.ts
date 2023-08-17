19546ae4a17f4aefc1df21bd7cc4bc0102481f9f
SecondThundeR <awayfromgalaxy@gmail.com>
2023-08-18 00:08:08 +0300

    style(commands): remove brackets for return one-liners

--- src/commands/weatherReset.ts
+++ src/commands/weatherReset.ts
@@ -10,15 +10,12 @@ export const weatherReset = new Composer();
 
 weatherReset.command("weatherreset", async (ctx) => {
   const userID = ctx.from?.id;
-  if (!userID) {
-    return ctx.reply(LOCALE.general.noUserID);
-  }
+  if (!userID) return ctx.reply(LOCALE.general.noUserID);
 
-  if (!hasSubscriptionData(userID)) {
+  if (!hasSubscriptionData(userID))
     return ctx.reply(LOCALE.weatherNotify.notSubscribed, {
       parse_mode: "MarkdownV2",
     });
-  }
 
   await removeSubscriber(userID);
   return ctx.reply(LOCALE.weatherNotify.successfullyUnsubscribed);
